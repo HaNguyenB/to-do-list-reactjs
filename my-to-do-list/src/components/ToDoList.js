12e9f7027b0018e5e17a8ccd551e392ac9416510
unknown <hnguyen3@oberlin.edu>
2023-06-11 13:32:32 +0700

    update filter

--- my-to-do-list/src/components/ToDoList.js
+++ my-to-do-list/src/components/ToDoList.js
@@ -1,30 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import ToDoCard from "./ToDoCard";
 
-//functional component
 const ToDoList = (props) => {
-    
-    const deleteCard = (id) => {
-        props.updateDeleted(id);
-    } 
+  const [filterOption, setFilterOption] = useState("all");
 
-    const checkCard = (id) => {
-        props.updateCompleted(id)
-    }
+  const deleteCard = (id) => {
+    props.updateDeleted(id);
+  };
 
-    const editCard = (value, id) => {
-        props.updateEdit(value, id)
+  const checkCard = (id) => {
+    props.updateCompleted(id);
+  };
+
+  const editCard = (value, id) => {
+    props.updateEdit(value, id);
+  };
+
+  const setFilter = (option) => {
+    setFilterOption(option);
+  };
+
+  const applyFilter = (card) => {
+    if (filterOption === "completed") {
+      return card.completed;
+    } else if (filterOption === "uncompleted") {
+      return !card.completed;
     }
-    
-    const renderToDoList = props.cards.map((card) => {
-        return (
-            <ToDoCard key = {card.id} card = {card} clickDelHandler = {deleteCard} clickDoneHandler = {checkCard} clickUpdateHandler = {editCard}></ToDoCard>
-        )
-    })
-    //return a map function that for each card object you have a ToDoCard element
-    return <div className = "ui todo list">
-        {renderToDoList}
+    return true;
+  };
+
+  const filteredCards = props.cards.filter(applyFilter);
+
+  const renderToDoList = filteredCards.map((card) => {
+    return (
+      <ToDoCard
+        key={card.id}
+        card={card}
+        clickDelHandler={deleteCard}
+        clickDoneHandler={checkCard}
+        clickUpdateHandler={editCard}
+      ></ToDoCard>
+    );
+  });
+
+  return (
+    <div className="ui todo list">
+      <div style = {{display: "flex", alignItems: "center", justifyContent: "space-evenly", padding: "10px"}}>
+        <label>
+          <input
+            type="radio"
+            name="filterOption"
+            checked={filterOption === "all"}
+            onChange={() => setFilter("all")}
+          />
+          Show all tasks
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="filterOption"
+            checked={filterOption === "completed"}
+            onChange={() => setFilter("completed")}
+          />
+          Show completed tasks
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="filterOption"
+            checked={filterOption === "uncompleted"}
+            onChange={() => setFilter("uncompleted")}
+          />
+          Show uncompleted tasks
+        </label>
+      </div>
+      {renderToDoList}
     </div>
-}
+  );
+};
 
-export default ToDoList
+export default ToDoList;
